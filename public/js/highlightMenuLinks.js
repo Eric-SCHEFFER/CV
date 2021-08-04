7999b184be72c08f90a07f6aa9ea8999bfa141ad
eric <eric.scheffer.pro@gmail.com>
2021-08-04 17:31:23 +0200

    highlightMenuLinks.js: Commencé fonction detecteScroll

--- public/js/highlightMenuLinks.js
+++ public/js/highlightMenuLinks.js
@@ -1,1 +1,14 @@
-alert ("coucou");
+// TODO: Mettre en surbrillance le menu correspondant à la position du scroll par rapport à son ancre
+
+window.addEventListener("DOMContentLoaded", () => {
+
+   document.querySelector("body").addEventListener("scroll", detecteScroll);
+
+   /**
+    * 
+    */
+   function detecteScroll() {
+      console.log(document.body.scrollTop);
+
+   }
+});

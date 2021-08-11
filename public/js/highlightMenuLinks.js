db4add2dcf204abda665df58d4f215bb079a06df
eric <eric.scheffer.pro@gmail.com>
2021-08-11 17:11:39 +0200

    highlightMenuLinks.js: Terminé

--- public/js/highlightMenuLinks.js
+++ public/js/highlightMenuLinks.js
@@ -1,17 +1,33 @@
 // TODO: Mettre en surbrillance le lien du menu quand l'ancre qu'il cible est affichée dans la page
 
 window.addEventListener("DOMContentLoaded", () => {
-
    const ancres = document.querySelectorAll(".ancre[id]");
-   const sections = document.querySelectorAll(".ancre[id] + section.bloc-1")
    document.querySelector("body").addEventListener("scroll", navHighlighter);
 
    /**
     * 
     */
    function navHighlighter() {
-      let scrollY = window.document.body.scrollTop;
-      console.log(scrollY);
+      let scrollY = document.body.scrollTop;
+      ancres.forEach(ancre => {
+         const sectionHeight = ancre.parentElement.offsetHeight;
+         const sectionTop = ancre.parentElement.offsetTop -90;
+         sectionId = ancre.getAttribute("id");
+         if (
+            scrollY > sectionTop &&
+            scrollY <= sectionTop + sectionHeight
+         ) {
+            document.querySelector(".navbar a[href*=" + sectionId + "]").classList.add("active");
+         } else {
+            document.querySelector(".navbar a[href*=" + sectionId + "]").classList.remove("active");
+         }
+
+      });
+
+
+
+
+
 
    }
 });

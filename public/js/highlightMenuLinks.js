3fa480d56fd1600f0fa580bee33c97b5d7c11b0a
eric <eric.scheffer.pro@gmail.com>
2021-08-12 10:56:03 +0200

    highlightMenuLinks.js: Terminé3

--- public/js/highlightMenuLinks.js
+++ public/js/highlightMenuLinks.js
@@ -1,8 +1,15 @@
-// Mettre en surbrillance le lien du menu quand l'ancre qu'il cible est affichée dans la page
+// Met en surbrillance le lien du menu quand l'ancre qu'il cible est affichée dans la page
 
 window.addEventListener("DOMContentLoaded", () => {
    const ancres = document.querySelectorAll(".ancre[id]");
-   document.querySelector("body").addEventListener("scroll", navHighlighter);
+   const fenetre = document.querySelector("body");
+   navHighlighter();
+   fenetre.addEventListener("scroll", navHighlighter);
+   // Au chargement de la page, si le scroll est tout en haut, le lien accueil doit être allumé
+   // if (fenetre.scrollTop === 0) {
+   //    document.querySelector(".navbar a[href*=top]").classList.add("active");
+   // }
+
 
    /**
     * 
@@ -10,8 +17,12 @@ window.addEventListener("DOMContentLoaded", () => {
    function navHighlighter() {
       let scrollY = document.body.scrollTop;
       ancres.forEach(ancre => {
-         const sectionHeight = ancre.parentElement.offsetHeight;
-         const sectionTop = ancre.parentElement.offsetTop -90;
+         let sectionHeight = ancre.parentElement.offsetHeight;
+         // Si l'id de l'ancre est "top", on agrandi la hauteur de sa section pour éviter qu'aucun lien ne soit allumé pendant la transition du scroll avec la prochaine section.
+         if (ancre.getAttribute("id") === "top") {
+            sectionHeight = ancre.parentElement.offsetHeight + 114;
+         }
+         const sectionTop = ancre.parentElement.offsetTop - 70;
          sectionId = ancre.getAttribute("id");
          if (
             scrollY > sectionTop &&

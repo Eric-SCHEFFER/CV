83c90fc4d88681a71628fdcdab3c387ac2e10a96
eric <eric.scheffer.pro>
2021-09-13 15:55:48 +0200

    Formulaire: Début2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,17 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         // TODO: Formulaire
         $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+            if (!isset($contact['motif']) && isset($_SERVER['HTTP_ORIGIN'])) {
+                $expediteur = $contact['email'];
+                $objet = $contact['objet'];
+                $destinataire = 'ericsch2e@gmail.com';
+                $templateTwig = "emails/contact.html.twig";
 
+                // Envoi du mail contenant les données du formulaire
+                $this->envoiEmail($mailer, $expediteur, $destinataire, $templateTwig, $objet, $contact);
+                $this->addFlash('succes', 'Le message à bien été envoyé');
+                return $this->redirectToRoute('home');
+            }
+        }
 
 
         $form = $this->createForm(ContactType::class);
